406c477e65f29a952d7915111de54b82da8d44e2
Ivan Paulino da Silva <ivanemmobile@gmail.com>
2020-08-21 15:28:57 -0300

    Create teste.py

---
+++ exercisePython/teste.py
@@ -0,0 +1,5 @@
+#fiz um programa que pergunta qual lingua quer aprender nesta pandemia!
+fala = str(input('qual sua lingua naterna:'))
+nfala = str(input('que lingua voce precisa aprender:'))
+
+print('Olá mundo!, você fala {} e quer aprender, {}'.format(fala, nfala))
