7be08abdbb13a8a405c81e2931248e584be53000
Ivan Paulino da Silva <ivanemmobile@gmail.com>
2020-09-05 00:16:32 -0300

    Aula 09 texto
    manupulando um texto

--- classePython/aula009.py
+++ classePython/aula009.py
@@ -1,1 +1,27 @@
-#Manupulando texto, 
+#Manipulando texto, usado para análise a len(frase) ; frase.count('o',0,13) -> conta o numero de letra 'o';
+#capitalize: primeira em mauscula; title: mudar tudo para as primeira maiuscula;
+#replace('python','android') substituir; upper: ira mudar para tudo em maiusculas; lower troca tudo em minusculas;
+#TRANSFORMAÇÃO -> REMOVER ESPAÇOS strip: remove espaços vazios, rstrip: remover as direita; lstrip: remove da esquerda
+#Divisão -> split():
+
+#prática
+frase = 'Curso em vídeo Python'
+#frase = frase.replace('Python', 'Android')
+
+#print(frase[3])
+#print(frase[3:13])
+#print(frase[:13])
+#print(frase[13:])
+#print(frase[1:13:2])
+#print(frase[::13])
+#print(frase.count('o'))
+
+#print("""Lorem Ipsum dolor sit amet, consectetuer adipiscing elit. Commodo aeneana Ligula eget dolor. Massa Aeneana
+#Consectetuer adipiscing elit. Alguns Link Aenean commodo ligula eget dolor. Massa Aeneana
+#Aenean massa.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean Commodo ligula eget dolor. Massa Aeneana
+#Lorem ipsum dolor sit Amet, consectetuer adipiscing elit. Aenean commodo ligula eget pain. Massagem Aeneana
+#Adipiscing Consectetuer elit. Commodo aeneana Ligula eget dolor. Massa Aeneana""")
+
+#print(frase.lower().find('vídeo'))
+
+
