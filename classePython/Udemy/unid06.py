7c1a90eaa39f341a59cdaa044ae13269c9438763
Ivan Paulino da Silva <ivanemmobile@gmail.com>
2021-10-26 22:52:33 -0300

    enviar aula06

--- classePython/Udemy/unid06.py
+++ classePython/Udemy/unid06.py
@@ -20,10 +20,31 @@ for numero in Lista:
 
 for numero in range(1,16):
     print(numero)
-    
+
 #serve para gerar uma tupla dentro do vetor
 for i, v in enumerate(nome):
     print(nome[i])
-"""
 
+qnt = int(input('Quantas vezes esse Loop deve rodar?: '))
+
+for n in range(1, qnt):
+    print(f'Imprimindo {n}')
+
+qnt = int(input('Quantas vezes esse Loop deve rodar?: '))
+soma = 0
+
+for n in range(1, qnt+1):
+    num = int(input(f' Informe o {n}/{qtd} valor: '))
+    soma = soma + num
+    print(f'A soma é {soma}')
+"""
+qnt = int(input('Quantas vezes esse Loop deve rodar?: '))
+soma = 0
+
+for n in range(1, qnt+1):
+    
+    num = int(input(f' Informe o {n}/{qnt} valor: '))
+    soma = soma + num
+print(f'A soma é {soma}')
+    
 #fim do programa
