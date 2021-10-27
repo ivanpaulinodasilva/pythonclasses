74131cfd71d9e08fdf62b253000d3463a0505347
Ivan Paulino da Silva <ivanemmobile@gmail.com>
2021-10-26 22:14:34 -0300

    Atualizando o conteudo
    aula 06 usando o for

---
+++ classePython/Udemy/unid06.py
@@ -0,0 +1,29 @@
+"""
+Estrutura de repetição
+sendo o for para exibir essa repetição;
+
+"""
+#variavel
+nome = 'Geek University - Criando Possibilidades'
+Lista = [1,2,3,4,5,6,7]
+num = range(1, 16)
+
+#executando o programa
+
+"""
+print('----#### Usando o Loop e o For ####----')
+for letra in nome:
+    print(letra)
+
+for numero in Lista:
+    print(numero)
+
+for numero in range(1,16):
+    print(numero)
+    
+#serve para gerar uma tupla dentro do vetor
+for i, v in enumerate(nome):
+    print(nome[i])
+"""
+
+#fim do programa
