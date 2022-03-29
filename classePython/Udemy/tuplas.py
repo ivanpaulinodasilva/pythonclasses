2d2d8cbd27339684c863badd4b9b129042d25408
Ivan Paulino da Silva <ivanemmobile@gmail.com>
2022-03-29 17:09:15 -0300

    aula dia 29-03 python

---
+++ classePython/Udemy/tuplas.py
@@ -0,0 +1,18 @@
+"""
+Tuplas são usadas e parecidas com listas
+Caracteristicas básicas 
+um usando parenteses ()
+dois toda tupla não muda quando criado mais de uma   
+
+"""
+
+tupla1 = (1, 2, 3, 4, 5, 6)
+tupla2 = (1,2,3,4,5,6,8)
+
+print(tupla1)
+
+print(type(tupla1))
+
+print(type(tupla2))
+
+
