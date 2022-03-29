8d29e05650d1c9ce9736ecefada55aed7962b943
Ivan Paulino da Silva <ivanemmobile@gmail.com>
2022-03-29 19:33:08 -0300

    concllusão de 30 mim de aula Tupla

--- classePython/Udemy/tuplas.py
+++ classePython/Udemy/tuplas.py
@@ -2,17 +2,60 @@
 Tuplas são usadas e parecidas com listas
 Caracteristicas básicas 
 um usando parenteses ()
-dois toda tupla não muda quando criado mais de uma   
-
-"""
-
+dois toda tupla não muda quando criado mais de uma 
+ 
 tupla1 = (1, 2, 3, 4, 5, 6)
-tupla2 = (1,2,3,4,5,6,8)
+tupla2 = (1, 2, 3, 4, 5, 6, 8)
 
 print(tupla1)
 
 print(type(tupla1))
 
+print(tupla2)
+
 print(type(tupla2))
 
+#tupla formada por rangue de 11 elementos
+print(tupla5)
+#definição de tipo de elemento
+print(type(tupla5))
 
+#pratica de desempacotamento de tupla
+tupla7 = ('Geek university', 'Programação em Python: Essencial')
+
+escola, curso = tupla7
+
+print(escola)
+print(curso)
+
+    #Aplicando opredadores de sistemas Max, Min, Sun, Largura
+
+print(sum(tupla2))
+print(max(tupla2))
+print(min(tupla2))
+print(len(tupla2))
+
+    #concatenar valores 
+ 
+print(type(tupla1))
+ 
+print(type(tupla4))
+ 
+print(tupla1 + tupla4)
+
+"""
+#Declaração de Variaveis
+
+tupla1 = (1, 2, 3, 4, 5, 6)
+tupla2 = (1, 2, 3, 4, 5, 6, 8)
+tupla4 = (7,)
+tupla5 = tuple(range(11))
+#pratica de desempacotamento de tupla
+tupla7 = ('Geek university', 'Programação em Python: Essencial')
+
+#Aplicação de Print dentro do programa
+    
+
+ 
+ 
+#Anotação somente por uma linha, continuação de assunto
