6f535cffb68be155315e4c4b66e773255826e9a1
Ivan Paulino <ivanemmobile@gmail.com>
2023-05-24 21:46:57 -0300

    Create dicionarios-mapa.py
    criando mapa-dicionario

---
+++ classePython/Udemy/dicionarios-mapa.py
@@ -0,0 +1,17 @@
+"""
+Mapa
+
+dicionario são relacionado como Chave/Valor.
+
+dicionario são representados por chaves {}.
+
+print(type({}))
+
+"""
+
+print('#### Dicionario - Mapa ####')
+
+paises = {'br': 'Brasil','eua': 'Estados Unidos', 'py': 'Paraguai'}
+
+print(paises)
+print(type(paises))
