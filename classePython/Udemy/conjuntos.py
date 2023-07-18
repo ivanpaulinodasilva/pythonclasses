470b496ca3fdc31c888cfe5ae47f55495bdec61d
Ivan Paulino <ivanemmobile@gmail.com>
2023-07-18 20:21:08 -0300

    Dicionário&&Conjunto
    Concluindo Dicionário

---
+++ classePython/Udemy/conjuntos.py
@@ -0,0 +1,7 @@
+"""
+Conjuntos, Teoria dos Conjuntos na Matématica.
+
+- em python são chamados em SET'S. não possuel duplicados ou ordenados.
+"""
+
+#Forma 01
--- classePython/Udemy/dicionarios-mapa.py
+++ classePython/Udemy/dicionarios-mapa.py
@@ -7,11 +7,69 @@ dicionario são representados por chaves {}.
 
 print(type({}))
 
-"""
-
 print('#### Dicionario - Mapa ####')
 
 paises = {'br': 'Brasil','eua': 'Estados Unidos', 'py': 'Paraguai'}
 
 print(paises)
 print(type(paises))
+
+pais = paises.get('ru', 'Não encontrado')
+
+if pais:
+    print(f'Encontrei o pais{paises}')
+
+else:
+    print('Não encontrado o Pais')
+
+
+    print('#### Dicionario - Mapa ####')
+
+paises = {'br': 'Brasil','eua': 'Estados Unidos', 'py': 'Paraguai'}
+
+# adicionar elementos em dicionário
+
+receita = {'jan': 100, 'fev': 120, 'mar': 300}
+print(receita)
+print(type(receita))
+
+#Add forma 01 <- Mais comum
+
+receita['abr'] = 300
+print(receita)
+
+#Add forma 02
+
+novo_dado={'mai': 500}
+receita.update(novo_dado)
+print(receita)
+
+#forma 01 de remover do dicionário
+
+receita = {'jan': 100, 'fev': 120, 'mar': 300}
+print(receita)
+print(type(receita))
+
+receita.pop('mar')
+print(receita)
+
+#forma 02
+
+del receita['fev']
+print(receita)
+
+"""
+
+#forma 01 de remover do dicionário
+
+receita = {'jan': 100, 'fev': 120, 'mar': 300}
+print(receita)
+print(type(receita))
+
+receita.pop('mar')
+print(receita)
+
+#forma 02
+
+del receita['fev']
+print(receita)
