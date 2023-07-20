227a46d853f10deef35fd39a7e6a473fc699715b
Ivan Paulino <ivanemmobile@gmail.com>
2023-07-19 23:27:22 -0300

    dsds
    dsdsd

--- classePython/Udemy/conjuntos.py
+++ classePython/Udemy/conjuntos.py
@@ -2,6 +2,32 @@
 Conjuntos, Teoria dos Conjuntos na Matématica.
 
 - em python são chamados em SET'S. não possuel duplicados ou ordenados.
+
+lista = [99, 2, 34, 23, 2, 12, 1, 44, 5, 34]
+print (f 'Lista: {lista}')
+
+tupla = 99, 2, 34, 23, 2, 12, 1, 44, 5, 34
+print(f 'Tupla: {tupla}')
+
+dicionario = {}.fromkeys([99, 2, 34, 23, 2, 12, 1, 44, 5, 34]) 
+print(f'Dicionario: {dicionario}')
+
+conjunto = {99, 2, 34, 23, 2, 12, 1, 44, 5, 34} 
+print(f'Conjuntos {conjunto}')
+
+
+
 """
 
-#Forma 01
+
+lista = [99, 2, 34, 23, 2, 12, 1, 44, 5, 34]
+print (f'Lista: {lista}')
+
+tupla = 99, 2, 34, 23, 2, 12, 1, 44, 5, 34
+print(f'Tupla: {tupla}')
+
+dicionario = {}.fromkeys([99, 2, 34, 23, 2, 12, 1, 44, 5, 34]) 
+print(f'Dicionario: {dicionario}')
+
+conjunto = {99, 2, 34, 23, 2, 12, 1, 44, 5, 34} 
+print(f'Conjuntos {conjunto}')
