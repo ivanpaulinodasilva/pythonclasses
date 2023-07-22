b1e81ae30c5f630465f370c496dca947d24e864a
Ivan Paulino <ivanemmobile@gmail.com>
2023-07-21 22:36:18 -0300

    Atualizacao-Conjunto
    atualizacao de treinamento

--- classePython/Udemy/conjuntos.py
+++ classePython/Udemy/conjuntos.py
@@ -15,19 +15,19 @@ print(f'Dicionario: {dicionario}')
 conjunto = {99, 2, 34, 23, 2, 12, 1, 44, 5, 34} 
 print(f'Conjuntos {conjunto}')
 
+s =  {1, 'b', True, 34, 21, 44}
+print(s)
+print(type(s))
 
+for valor in s :
+    print(valor)
+
+# aplicações do uso do set
+
+cidades =  ['Belo Horizonte', 'São Paulo', 'Campo Grande', 'Cuiabá', 'Campo Grande', 'São Paulo', 'Cuiabá']
+print(cidades)
+print(len(cidades))
+
+print(len(set(cidades)))    
 
 """
-
-
-lista = [99, 2, 34, 23, 2, 12, 1, 44, 5, 34]
-print (f'Lista: {lista}')
-
-tupla = 99, 2, 34, 23, 2, 12, 1, 44, 5, 34
-print(f'Tupla: {tupla}')
-
-dicionario = {}.fromkeys([99, 2, 34, 23, 2, 12, 1, 44, 5, 34]) 
-print(f'Dicionario: {dicionario}')
-
-conjunto = {99, 2, 34, 23, 2, 12, 1, 44, 5, 34} 
-print(f'Conjuntos {conjunto}')
