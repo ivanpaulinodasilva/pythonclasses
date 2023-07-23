0b5f35eb890b496039e72818f10a04ec2c90132f
Ivan Paulino <ivanemmobile@gmail.com>
2023-07-23 13:15:00 -0300

    create_funcao
    criando funções em python

---
+++ classePython/Udemy/def_func.py
@@ -0,0 +1,43 @@
+"""
+todo os aplicativos usam funções algumas mais especificas do que as outras
+#exemplo 01
+
+cores = ['preto', 'vermelho', 'amarelo', 'azul']
+
+print(cores)
+
+text = 'professores'
+
+print(text)
+
+cores.append('verde')
+
+print(cores)
+
+def cores():
+    print('preto')
+    print('vermelho')
+    print('amarelo')
+    print('azul')
+    print('vermelho')
+    print('amarelo')
+    print('azul')
+
+cores()
+
+#seis linha para iniciar os comentarios
+"""
+print('####### Definindo Funções #######')
+
+
+
+def cores():
+    print('preto')
+    print('vermelho')
+    print('amarelo')
+    print('azul')
+    print('vermelho')
+    print('amarelo')
+    print('azul')
+
+cores()
