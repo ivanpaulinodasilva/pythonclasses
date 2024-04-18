5c88a14b67be184e0d98e937aa2b5f751fe3dc54
Ivan Paulino <ivanemmobile@gmail.com>
2024-04-18 15:14:47 -0300

    Create firstpgrinpy.py
    Desenvolvendo para uso de Python.

---
+++ learnPython/firstpgrinpy.py
@@ -0,0 +1,49 @@
+from datetime import date
+
+##Colocar um bloco de comentario
+#sum = 1 + 2
+
+#print(sum)
+
+#sum = 1 + 3
+
+#product = sum * 2
+
+#print(product)
+
+#left_side = 10
+#right_side = 5
+#left_side / right_side # 2
+
+#date.today()
+
+#print(date.today())
+
+#print("Today's date is: " + str(date.today()))
+
+###Criando o projeto
+###
+#parsecs = 11
+#lightyears = parsecs * 3.26
+#print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
+
+###Acessando o Programa
+#print("Welcome to the greeter program")
+#name = input("Enter your name: ")
+#print("Greetings " + name)
+
+
+###Criando uma calculadora
+#print("calculator program")
+#first_number = input("first number: ")
+#second_number = input("second number: ")
+#print(first_number + second_number)
+
+
+#inserindo dados de navegação
+parsecs_input = input("Input number of parsecs:")
+parsecs = int(parsecs_input)
+lightyears = 3.26156 * parsecs
+
+print(parsecs_input + " parsecs is " + str(lightyears) + " lightyears")
+
